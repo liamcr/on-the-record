c222b5450413be3ce21cb1746b875a6f707b700d
lcrocket <liamcrocket@gmail.com>
2023-09-17 18:03:08 -0400

    Add sliding button component

--- components/SlidingButton/SlidingButton.tsx
+++ components/SlidingButton/SlidingButton.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import styles from "./SlidingButton.module.css";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { SvgIconTypeMap } from "@mui/material";
 import Body from "../Body/Body";
+import { body } from "../..//common/fonts";
 
 interface SlidingButtonProps {
+  /**
+   * MUI icon to render next to the text
+   */
   icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+  /**
+   * Icon font size
+   */
   iconSize: string;
+  /**
+   * Text to display next to the icon
+   */
   text: string;
+  /**
+   * Font size of the text
+   */
   textSize: string;
+  /**
+   * Function to run when the button is clicked
+   *
+   * @returns
+   */
+  onClick: () => any;
+  /**
+   * Is the button going to slide in/out on hover?
+   */
+  animated?: boolean;
 }
 
 const SlidingButton: React.FC<SlidingButtonProps> = ({
@@ -17,12 +40,42 @@ const SlidingButton: React.FC<SlidingButtonProps> = ({
   iconSize,
   text,
   textSize,
+  onClick,
+  animated: interactive = true,
 }) => {
   const Icon = icon;
+  const [isHovering, setIsHovering] = useState(false);
+  const textRef = useRef<HTMLParagraphElement>(null);
+
+  const onMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const onMouseExit = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <div className={styles.slidingButton}>
+    <div
+      className={styles.slidingButton}
+      style={{
+        width:
+          isHovering || !interactive
+            ? `calc(${iconSize} + ${textRef.current?.offsetWidth}px)`
+            : iconSize,
+      }}
+      onMouseEnter={interactive ? onMouseEnter : () => {}}
+      onMouseLeave={interactive ? onMouseExit : () => {}}
+      onClick={onClick}
+    >
       <Icon style={{ fontSize: iconSize }} />
-      <Body content={text} style={{ fontSize: textSize }} />
+      <p
+        style={{ fontSize: textSize }}
+        ref={textRef}
+        className={`${body.className} ${styles.text}`}
+      >
+        {text}
+      </p>
     </div>
   );
 };
--- stories/SlidingButton.stories.tsx
+++ stories/SlidingButton.stories.tsx
@@ -4,6 +4,7 @@ import TestProfilePic from "./assets/TestProfilePic.jpg";
 import SlidingButton from "../components/SlidingButton/SlidingButton";
 import { EntityType } from "../common/types";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import RateReviewOutlinedIcon from "@mui/icons-material/RateReviewOutlined";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -20,11 +21,39 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const List: Story = {
   args: {
     icon: ListOutlinedIcon,
     iconSize: "4rem",
     text: "List",
     textSize: "3rem",
+    onClick: () => {
+      console.log("Clicked");
+    },
+  },
+};
+
+export const Review: Story = {
+  args: {
+    icon: RateReviewOutlinedIcon,
+    iconSize: "4rem",
+    text: "Review",
+    textSize: "3rem",
+    onClick: () => {
+      console.log("Clicked");
+    },
+  },
+};
+
+export const NotInteractive: Story = {
+  args: {
+    icon: RateReviewOutlinedIcon,
+    iconSize: "4rem",
+    text: "Review",
+    textSize: "3rem",
+    animated: false,
+    onClick: () => {
+      console.log("Clicked");
+    },
   },
 };
