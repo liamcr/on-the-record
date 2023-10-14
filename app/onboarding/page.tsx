8767e17406167f118fd26f2262f2e47dfa238b10
lcrocket <liamcrocket@gmail.com>
2023-10-14 18:15:26 -0400

    WIP onboarding page

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import styles from "./page.module.css";
+import Heading from "../../components/Heading/Heading";
+
 export default function Onboarding() {
-  return <div>We are onboarding</div>;
+  return (
+    <div className={styles.pageContainer}>
+      <Heading component="h1" content="Welcome" />
+      <Heading
+        component="h2"
+        content="Welcome to On The Record! Let's get you set up."
+      />
+      <Heading component="h2" content="What's your name?" />
+    </div>
+  );
 }
